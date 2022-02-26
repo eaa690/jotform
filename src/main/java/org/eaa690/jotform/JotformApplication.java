ac04036a87ba47a68dbd5bbc09a58ba5879c430b
Brian Michael <bsmichae42@yahoo.com>
2022-02-26 08:26:52 -0500

    Initial checkin

---
+++ src/main/java/org/eaa690/jotform/JotformApplication.java
@@ -0,0 +1,13 @@
+package org.eaa690.jotform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JotformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JotformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/eaa690/jotform/JotformApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eaa690.jotform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JotformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
